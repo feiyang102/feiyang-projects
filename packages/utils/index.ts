929e21bda92df179e043280621a30f9066a0b6e4
feiyang102 <feiyang102@outlook.com>
2024-01-28 14:52:57 +0800

    reature: 局部引入lodash,全局引入typescript

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,4 +1,10 @@
+import _ from "lodash";
+
 export function getTimestamp(count: number) {
   console.log(count);
   console.log(Date.now());
+}
+
+export function add(a: number, b: number) {
+  console.log(_.add(a, b));
 }
