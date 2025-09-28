f5a115187756e9140c3c0f4d0850e8e6063f0cfb
feiyang102 <feiyang102@outlook.com>
2025-09-28 22:42:16 +0800

    feat: 引入naive-ui

--- apps/vue3-demo/src/views/HomeView.vue
+++ apps/vue3-demo/src/views/HomeView.vue
@@ -7,5 +7,15 @@ import ReuseableTemplate from '../components/ReuseableTemplate/ReuseableTemplate
   <main>
     <!-- <TheWelcome /> -->
     <ReuseableTemplate />
+    <n-button type="primary"> Primary </n-button>
+    <n-space>
+      <n-button>Default</n-button>
+      <n-button type="tertiary"> Tertiary </n-button>
+      <n-button type="primary"> Primary </n-button>
+      <n-button type="info"> Info </n-button>
+      <n-button type="success"> Success </n-button>
+      <n-button type="warning"> Warning </n-button>
+      <n-button type="error"> Error </n-button>
+    </n-space>
   </main>
 </template>
--- apps/vue3-demo/vite.config.ts
+++ apps/vue3-demo/vite.config.ts
@@ -3,16 +3,35 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 1、引入自动导入插件
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // 2、引入自动组件插件
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification'],
+        },
+      ],
+      dts: fileURLToPath(new URL('./types/auto-imports.d.ts', import.meta.url)),
+    }),
+    // 3、引入自动组件插件
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dts: fileURLToPath(new URL('./types/components.d.ts', import.meta.url)),
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
